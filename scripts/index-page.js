3aaec142a0edf139d712cc69dda72e0c9476019e
MrRobotical <mcleanj901@gmail.com>
2024-02-22 20:27:13 -0500

    dynamic comments integration

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,37 +1,90 @@
+let commentsArr = [
+  {
+    name: 'Victor Pinto',
+    timestamp: '11/02/2023',
+    text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
+  },
+  {
+    name: 'Christina Cabrera',
+    timestamp: '10/28/2023',
+    text: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
+  },
+  {
+    name: 'Isaac Tadesse',
+    timestamp: '10/20/2023',
+    text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
+
+// My Form event listener
 document.querySelector('#form').addEventListener('submit', function (e) {
   e.preventDefault();
 
   const name = document.querySelector('#name').value;
   const comment = document.querySelector('#comment').value;
 
-  // Grabing the parent containers here
-  const nameParentContainer = document.querySelector(
-    '.comments__name-date-container'
-  );
+  // Clear Inputs
+  document.querySelector('#name').value = '';
+  document.querySelector('#comment').value = '';
 
-  const avatarParentContainer = document.querySelector('.comments__post-new');
+  // Time Stamp attempt - Not sure about this
+  const timestamp = new Date().toLocaleDateString('en-US');
 
-  const commentsParentContainer = document.querySelector('.comments__body');
+  const newComment = {
+    name: name,
+    timestamp: timestamp,
+    text: comment,
+  };
 
-  //
-  //   console.log(avatarParentContainer);
+  // '11/02/2023',
 
-  const createNameOutputEl = document.createElement('p');
-  createNameOutputEl.classList.add('comments__username');
-  nameParentContainer.appendChild(createNameOutputEl);
-  createNameOutputEl.innerText = name;
+  commentsArr.push(newComment);
+  console.log(commentsArr);
 
-  const createDateOutputEl = document.createElement('p');
-  createDateOutputEl.classList.add('comments__date');
-  nameParentContainer.appendChild(createDateOutputEl);
-  createDateOutputEl.innerText = '02/11/24';
+  //First El to append to:
+  const createJsCommentsParentContainer =
+    document.querySelector('.comments__posts');
 
-  const createCommentOutputEl = document.createElement('p');
-  createCommentOutputEl.classList.add('comments__txt');
-  commentsParentContainer.appendChild(createCommentOutputEl);
-  createCommentOutputEl.innerText = comment;
+  //Avatar Container via JS
+  const createJsAvatarContainer = document.createElement('div');
+  createJsAvatarContainer.classList.add('comments__post-new');
+  createJsCommentsParentContainer.prepend(createJsAvatarContainer);
 
+  // Avatar El via JS
   const createAvatarOutputEl = document.createElement('div');
   createAvatarOutputEl.classList.add('comments__avatar-loggedIn');
-  avatarParentContainer.appendChild(createAvatarOutputEl);
+  createJsAvatarContainer.prepend(createAvatarOutputEl);
+
+  //Comments Body Container via JS
+  const createJsbodyContainer = document.createElement('div');
+  createJsbodyContainer.classList.add('comments__body');
+  createJsAvatarContainer.appendChild(createJsbodyContainer);
+
+  //Name - Date Container via JS
+  const createJsNameDateContainer = document.createElement('div');
+  createJsNameDateContainer.classList.add('comments__name-date-container');
+  createJsbodyContainer.appendChild(createJsNameDateContainer);
+
+  // Name El via JS
+  const createNameOutputEl = document.createElement('p');
+  createNameOutputEl.classList.add('comments__username');
+  createJsNameDateContainer.appendChild(createNameOutputEl);
+  createNameOutputEl.innerText = name;
+
+  // Date
+  const createDateOutputEl = document.createElement('p');
+  createDateOutputEl.classList.add('comments__date');
+  createJsNameDateContainer.appendChild(createDateOutputEl);
+  createDateOutputEl.innerText = timestamp;
+
+  //Comments
+  const createCommentOutputEl = document.createElement('p');
+  createCommentOutputEl.classList.add('comments__txt');
+  createJsbodyContainer.appendChild(createCommentOutputEl);
+  createCommentOutputEl.innerText = comment;
+
+  // Divider Line -
+  const dividerLinesEl = document.createElement('div');
+  dividerLinesEl.classList.add('comments__divider-lines');
+  createJsCommentsParentContainer.prepend(dividerLinesEl);
 });
